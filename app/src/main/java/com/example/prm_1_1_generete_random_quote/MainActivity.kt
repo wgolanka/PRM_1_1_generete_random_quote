4e2551a8155a74b33fd76cd15949cfb82a109fb3
wgolanka <wiktoria.golanka@gmail.com>
2020-03-09 06:30:38 +0100

    Add empty onClick method for button

--- app/src/main/java/com/example/prm_1_1_generete_random_quote/MainActivity.kt
+++ app/src/main/java/com/example/prm_1_1_generete_random_quote/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.prm_1_1_generete_random_quote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickShowRandomQuotation(view: View) {
+
     }
 }
