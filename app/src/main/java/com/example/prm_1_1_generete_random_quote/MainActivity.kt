c4ba958891ac2f0e333824d168cfa8cbff004d93
wgolanka <wiktoria.golanka@gmail.com>
2020-03-09 06:27:43 +0100

    git init

---
+++ app/src/main/java/com/example/prm_1_1_generete_random_quote/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prm_1_1_generete_random_quote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
