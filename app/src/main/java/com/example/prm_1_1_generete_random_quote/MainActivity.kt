de4a80129022657c6c9a2f40a923a26f7daaa908
wgolanka <wiktoria.golanka@gmail.com>
2020-03-09 07:05:53 +0100

    Add logic to onclick method to show random quotation

--- app/src/main/java/com/example/prm_1_1_generete_random_quote/MainActivity.kt
+++ app/src/main/java/com/example/prm_1_1_generete_random_quote/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.prm_1_1_generete_random_quote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickShowRandomQuotation(view: View) {
+        val quotations = listOf(
+                "Dwa projekty. Do piątku do północy.",
+                "Nie chce słyszeć:\nNie wiem",
+                "Praca ładna i wychuchana",
+                "Late delivery - na 50% punktów",
+                "Nawet jak się nie uda wszystkiego to i tak spróbować oddać",
+                "Na następne zajęcia każdy w domu zainstalował android studio",
+                "Ważne są śmieszne obrazki",
+                "50% sumy punktów potrzebna do zaliczenia.",
+                "Proszę nie przeginać",
+                "Nie zabijać emulatora za każdym razem.")
 
+        val quotation : TextView = findViewById(R.id.quotation)
+
+        val randomQuotation = quotations[Random.nextInt(0, quotations.size -1)]
+        quotation.text = randomQuotation
     }
 }
